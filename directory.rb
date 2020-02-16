778a08b243d6da849e103ece5c3ab2c18d47a92e
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-02-16 15:59:43 +0000

    Saving the data to a file

--- directory.rb
+++ directory.rb
@@ -1,5 +1,17 @@
 @students = [] #an empty array accessible to all methods
 
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+  
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -27,6 +39,7 @@ def print_menu
   #1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" #9 because we'll be adding more items
 end
 
@@ -43,6 +56,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit #this will cause the program to terminate"
   else
@@ -60,7 +75,7 @@ def print_student_list
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-def print_footer(students)
+def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
